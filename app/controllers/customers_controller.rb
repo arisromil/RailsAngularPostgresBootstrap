5ae4ff84b73290fcfe63de6468c0dfa4cd303b89
arisromil <arisromil@yahoo.com>
2016-06-05 14:45:44 -0600

    Using Materialized Views for Better Performance

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -25,9 +25,9 @@ PAGE_SIZE = 10
     end
 
   def show
-    customer = Customer.find(params[:id])
+    customer_detail = CustomerDetail.find(params[:id]) 
     respond_to do |format|
-      format.json { render json: customer }
+      format.json { render json: customer_detail }
     end
   end
 end
